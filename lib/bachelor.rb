36c8357e4a3accd41e767d2aea49804c59a7f554
Raymond Chow <raychow1944@yahoo.com>
2018-12-25 09:19:39 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -9,7 +9,7 @@ def get_contestant_name(data, occupation)
   # code here
   data.each do |season, contestants|
     contestants.select do |contestant|
-      conte
+      contestant["occupation"] == occupation
     end
   end
 end
